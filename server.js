044d602b27724ed7fca2b65d093ce30570ac9ab9
MatheusTomazella <matheustomazella250504@gmail.com>
2020-10-30 21:35:20 -0300

    deploy debug 2

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-require('dotenv/config');
+require('dotenv').config();
+const process   = require( 'process' );
 const { app }   = require( './app/app' );
 const http      = require( 'http' ).Server( app );
 const config    = require( './config/config.json' );
 
-http.listen( process.env.PORT || config.PORT    , ( error ) => {
+http.listen( process.env.PORT || config.PORT, ( error ) => {
     if ( error ) throw error;
     console.log( 'Server Running' );
 } );
