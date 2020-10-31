833c5fb8c1966baf392fd3bffab680fef852bd65
MatheusTomazella <matheustomazella250504@gmail.com>
2020-10-30 21:24:35 -0300

    deploy debug 1

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const { env } = require('process');
+const { env }   = require('process');
 const { app }   = require( './app/app' );
 const http      = require( 'http' ).Server( app );
 const config    = require( './config/config.json' );
 
-http.listen( config.PORT || env.PORT, ( error ) => {
+http.listen( process.env.PORT || config.PORT    , ( error ) => {
     if ( error ) throw error;
     console.log( 'Server Running' );
 } );
